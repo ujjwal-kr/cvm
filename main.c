cf525abb1b21fff39bff8cbc2e2a7026f30f5ab7
Ujjwal Kumar <epicujjwal@protonmail.com>
2023-11-11 18:35:15 +0530

    better malloc example

--- main.c
+++ main.c
@@ -2,9 +2,24 @@
 
 int main(void)
 {
-    Object obj = {data : NULL, size : 69};
-    obj.data = malloc(obj.size);
-    obj.data = "Hello world\n";
-    printf("%s", (char*) obj.data);
+    uint16_t random_bytes[100];
+    getrandom(&random_bytes, sizeof(random_bytes), 0);
+    Object *objs[100];
+    for (int i = 0; i < 100; i++)
+    {
+        // Object obj = {data : NULL, size : random_bytes[i]};
+        // obj.data = malloc(obj.size);
+        // objs[i] = &obj;
+
+        Object *obj = malloc(sizeof(Object));
+        obj->data = malloc(random_bytes[i]);
+        obj->size = random_bytes[i];
+        objs[i] = obj;
+    }
+
+    for (int i = 0; i < 100; i++)
+    {
+        printf("Size of Object %d is %ld \n", i, objs[i]->size);
+    }
     return 0;
 }
--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#include <sys/random.h>
+#include <string.h>
 
 int add(int a, int b);
 
