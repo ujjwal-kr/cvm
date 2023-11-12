6733d55e0a873db2c22da5f4bff9d04188b17912
Ujjwal Kumar <epicujjwal@protonmail.com>
2023-11-12 22:06:04 +0530

    experimental pointer aritchmetic

--- main.c
+++ main.c
@@ -5,15 +5,15 @@ int main(void)
     uint8_t random_bytes[100];
     getrandom(&random_bytes, sizeof(random_bytes), 0);
     Object *objs[100];
+    Object *obj = malloc(sizeof(Object) * 100);
     for (int i = 0; i < 100; i++)
     {
-        Object *obj = malloc(sizeof(Object));
         assert(obj != NULL);
-        obj->data = malloc(random_bytes[i] + 1);
-        assert(obj->data != NULL);
-        strcpy(obj->data, "H\n");
-        obj->size = random_bytes[i] + 1;
-        objs[i] = obj;
+        (obj + i)->data = malloc(random_bytes[i] + 1);
+        assert((obj + i)->data != NULL);
+        strcpy((obj + i)->data, "H\n");
+        (obj + i)->size = random_bytes[i] + 1;
+        objs[i] = (obj + i);
     }
 
     printf("%p\n", objs[69]->data);
@@ -22,11 +22,11 @@ int main(void)
     Object *o = (struct Object *)objs[69];
     printf("%p\n", o->data);
 
-    for (int i = 0; i < 100; i++) 
-    {
-        // swapping these returns a segfault
-        free(objs[i]->data);
-        free(objs[i]);
-    }
+    // for (int i = 0; i < 100; i++)
+    // {
+    //     // swapping these results in a segfault
+    //     free(objs[i]->data);
+    //     free(objs[i]);
+    // }
     return 0;
 }
