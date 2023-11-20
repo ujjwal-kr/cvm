a29d8ea0ae33067f052d468a095967ca2917d398
Ujjwal Kumar <epicujjwal@protonmail.com>
2023-11-20 19:25:28 +0530

    free: get free memory chunks

--- mylibc/mem.h
+++ mylibc/mem.h
@@ -8,6 +8,7 @@ typedef struct
 {
     size_t capacity;
     size_t size;
+    size_t chunks;
     void *start;
 } Segment;
 
@@ -20,6 +21,7 @@ void new_segment()
     Segment segment;
     segment.capacity = SEGMENT_SIZE;
     segment.size = 0x0;
+    segment.chunks = 0;
     segment.start = mmap(
         NULL,
         segment.capacity,
@@ -51,6 +53,40 @@ Segment *latest_segment(size_t size, char *segment_idx)
     return &segments[segment_count];
 }
 
+void get_chunk_props(void *ptr, char *size, char *free, char *index)
+{
+    char *s = ptr - sizeof(char);
+    char *f = ptr - sizeof(char) * 2;
+    char *i = ptr - sizeof(char) * 3;
+    *size = (char)*s;
+    *free = (char)*f;
+    *index = (char)*i;
+}
+
+// returns pointer to free space in memory; if the chunk has the desired size
+void *get_free_mem(size_t size)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Segment segment = segments[i];
+        if (segment.size == 0)
+        {
+            return segment.start;
+        }
+        size_t offset = 0;
+        for (int j = 0; j < segment.chunks; j++)
+        {
+            char chunk_size, free, index;
+            get_chunk_props(segment.start + offset, &chunk_size, &free, &index);
+            if (free == 1 && chunk_size <= size) {
+                return segment.start + offset;
+            }
+            offset += chunk_size;
+        }
+    }
+    return NULL;
+}
+
 // returns a pointer to the beginning of the chunk in segment
 void *add_chunk(size_t size, Segment *segment, char segment_idx)
 {
@@ -60,6 +96,7 @@ void *add_chunk(size_t size, Segment *segment, char segment_idx)
     s[1] = 0; // free bit
     s[2] = size;
     segment->size += size + sizeof(char) * 3;
+    segment->chunks += 1;
     return start + sizeof(char) * 3;
 }
 
@@ -74,21 +111,12 @@ void *malloc(size_t size)
     return add_chunk(size, latest, segment_idx);
 }
 
-void get_chunk_props(void *ptr, char *size, char *free, char *index)
-{
-    char *s = ptr - sizeof(char);
-    char *f = ptr - sizeof(char) * 2;
-    char *i = ptr - sizeof(char) * 3;
-    *size = (char)*s;
-    *free = (char)*f;
-    *index = (char)*i;
-}
-
 void free(void *ptr)
 {
     char size, free, index;
     get_chunk_props(ptr, &size, &free, &index);
-    if (free == 1) return;
+    if (free == 1)
+        return;
     char *f = ptr - sizeof(char) * 2;
     *f = 1;
 }
